f5d6e7a9302413111a7bb086ac879bb518fc1ea9
satyam kumar shah <satyamshah1234567890@gmail.com>
2024-01-05 10:20:00 +0530

    delete:

--- BackgroundChanger/JS/script.js
+++
@@ -1,8 +0,0 @@
-function changeBG(clr){
-    if (clr=='black') {
-        document.body.style.color='white';
-    }else{
-        document.body.style.color="unset";
-    }
-    document.body.style.backgroundColor=clr;
-}
